3ece6b9b746b5bbc60df5cdbe2b8307e1dc28ee9
XicoCorreia <xicocorreia17@gmail.com>
2024-12-04 11:54:17 +0100

    Jumps figure out

--- examples/count_bits.asm
+++ examples/count_bits.asm
@@ -10,8 +10,9 @@
     ja +4
     mov r2, r0       ; temp = num
     and r2, 1        ; temp = num & 1
-    je r2, +2        ; skip if temp == 0
+    jeq r2, 0, +2        ; skip if temp == 0
     add r1, 1        ; count = count + 1
-    shr r0, 1        ; num = num >> 1
+    mov r1, 2       ; Load the divisor (2) into r1
+    div r0, r1      ; Divide r0 by r1 (i.e., r0 / 2) and store the result back in r0
     jgt r0, 0, +-5   ; while num > 0
     exit
